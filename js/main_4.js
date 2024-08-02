7b0edb6e42359af7764b083cf7c26a2cc3216bf0
qingfangcomputer <155035952+qingfangcomputer@users.noreply.github.com>
2024-08-02 10:11:53 +0800

    Delete js directory

--- js/main_4.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-	$('.navbar-nav a[href],a[role="gun"]').click(function(){
-		$("html, body").animate({scrollTop: $($(this).attr('href')).offset().top});
-		$('.navbar-nav li').removeClass('active');
-		$(this).parent().addClass('active');
-	});
-});
